8b4150767cdee4281b4053356ffe229877b39f6f
bobbigmac <admin@bobbigmac.com>
2021-05-05 16:43:21 +0100

    Testing serversi.de

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
-window.addEventListener('DOMContentLoaded', function() {
+import ServerSide from 'https://serversi.de/api.js';
+// import ServerSide from '//localhost:4000/api.js';
+
+const apiKey = 'some-key-abc123';
+const serverUrl = localStorage['use-local-api'] ? 'http://localhost:4000/' : 'https://serversi.de/';
+const server = new ServerSide({ apiKey, server: serverUrl });
+
+window.addEventListener('DOMContentLoaded', async function() {
 	console.log('Loaded DOM')
+	try {
+		const result = await server.test()
+		console.log({ result })
+	} catch(exc) {
+		console.error({ exc });
+	}
 })
